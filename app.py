d78a3700ec3ec08c5063334d02c4495f134a99fb
Shan Ali Shah Sayed <shanalishah7@hotmail.com>
2026-03-13 21:28:01 -0500

    Increase footer margin from 40px to 120px

--- app.py
+++ app.py
@@ -954,7 +954,7 @@ def render_footer():
         """
         <style>
         .custom-footer {
-            margin-top: 40px;
+            margin-top: 120px;
             text-align: center;
             color: gray;
             font-size: 0.9rem;
